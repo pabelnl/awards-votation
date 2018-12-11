45bdf8cc57b380f33162a63e06ca0d69d81b9710
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-12-11 11:34:30 -0400

    removed optionals

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -130,27 +130,27 @@ class HomeController < ApplicationController
       if params[:active] = "ok"
         @mmgvoWinner = Voter.select("COUNT(mmgvo) as total, mmgvo").group(:mmgvo).having("COUNT(mmgvo) > 1").order(:mmgvo).map{|p| {p.mmgvo => p.total} }
         if not @mmgvoWinner.first.nil?
-          @mmgvoWinnerCount = @mmgvoWinner.first.first.second ?? "0"
+          @mmgvoWinnerCount = @mmgvoWinner.first.first.second
         end
 
-        @revelacionesWinner = Voter.select("COUNT(revelacion) as total, revelacion").group(:revelacion).having("COUNT(revelacion) > 1").order(:revelacion).map{|p| {p.revelacion => p.total} }
+        @revelacionesWinner = Voter.select("COUNT(revelacion) as total, revelacion").group(:revelacion).having("COUNT(revelacion) > 1").order(:revelacion).reverse_order.map{|p| {p.revelacion => p.total} }
         if not @revelacionesWinner.first.nil?
-          @revelacionesWinnerCount = @revelacionesWinner.first.first.second ?? "0"
+          @revelacionesWinnerCount = @revelacionesWinner.first.first.second
         end
 
         @infelicesWinner = Voter.select("COUNT(infeliz) as total, infeliz").group(:infeliz).having("COUNT(infeliz) > 1").order(:infeliz).map{|p| {p.infeliz => p.total} }
         if not @infelicesWinner.first.nil?
-          @infelicesWinnerCount = @infelicesWinner.first.first.second ?? "0"
+          @infelicesWinnerCount = @infelicesWinner.first.first.second
         end
 
         @jugadorWinner = Voter.select("COUNT(jugador) as total, jugador").group(:jugador).having("COUNT(jugador) > 1").order(:jugador).map{|p| {p.jugador => p.total} }
         if not @jugadorWinner.first.nil?
-          @jugadorWinnerCount = @jugadorWinner.first.first.second ?? "0"
+          @jugadorWinnerCount = @jugadorWinner.first.first.second
         end
 
         @nuncaWinner = Voter.select("COUNT(nunca) as total, nunca").group(:nunca).having("COUNT(nunca) > 1").order(:nunca).map{|p| {p.nunca => p.total} }
         if not @nuncaWinner.first.nil?
-          @nuncaWinnerCount = @nuncaWinner.first.first.second ?? "0"
+          @nuncaWinnerCount = @nuncaWinner.first.first.second
         end
 
         @votes = Voter.where(confirmed: true)
