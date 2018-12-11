12aa806f74989a384c5261c8f413532e405146fb
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-12-10 22:46:50 -0400

    Added heroku url

--- app/mailers/voter_mailer.rb
+++ app/mailers/voter_mailer.rb
@@ -3,8 +3,8 @@ class VoterMailer < ApplicationMailer
 
   def voter_email(voter)
       @voter = voter
-      @base_url = request.base_url
-      
+      @base_url = ENV['HEROKU_URL']
+
       if @voter.mmgvo.present?
         attachments.inline["mmgvo.png"] = File.read("#{Rails.root}/public/lib/imgs/profiles/"+@voter.mmgvo.downcase+".png")
       end
